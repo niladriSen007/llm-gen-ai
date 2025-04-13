22d9a9883fce3140106f6742e8d94b411a77af8d
Niladri <niladrisengit12@gmail.com>
2025-04-13 10:28:10 +0530

    first commit

---
+++ magic.py
@@ -0,0 +1,2 @@
+'def add_numbers(a, b):\n    return a + b\n\nresult = add_numbers(3, 5)\nprint(f"The result of adding 3 and 5 is {result}.")' 
+'\n# This program adds two numbers and prints the result' 
